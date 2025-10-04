a0f3ff38958af16aed2cb1036e9d8a3dcc3cc69a
ray201ss <turki95r@gmail.com>
2025-10-04 07:06:42 +0000

    fix: correct 101-keygen code

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -2,19 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - generates a random valid password
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int sum, i;
-	char c;
+    int sum;
+    char c;
 
-	srand(time(NULL));
-	sum = 0;
-	while (sum < 2645)
-	{
-		c = rand() % 94 + 33;
-		sum += c;
-		putchar(c);
-	}
-	putchar(2772 - sum);
-	return (0);
+    srand(time(NULL));
+    sum = 0;
+
+    while (sum <= 2645)
+    {
+        c = rand() % 128;
+        sum += c;
+        putchar(c);
+    }
+    putchar(2772 - sum);
+
+    return (0);
 }
