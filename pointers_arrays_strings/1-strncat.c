495bf0038d74beef2aafe86b6ec44b3d24ce4cea
ray201ss <ray201ss@example.com>
2025-10-04 07:53:57 +0000

    Fix header and implement _strncat without non-ASCII chars

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,32 +1,26 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings using at most n bytes from src
+ * _strncat - concatenates two strings, using at most n bytes from src
  * @dest: destination string
  * @src: source string
  * @n: maximum number of bytes to use from src
- *
  * Return: pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
+	int i = 0, j = 0;
 
-	i = 0;
 	while (dest[i] != '\0')
-	{
 		i++;
-	}
 
-	j = 0;
 	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	dest[i] = '\0';
 
+	dest[i] = '\0';
 	return (dest);
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,21 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
 char *_strcat(char *dest, char *src);
-cd ~/holbertonschool-low_level_programming
-git add .
-git commit -m "Add 1-strncat.c"
-git push origin main
 char *_strncat(char *dest, char *src, int n);
-#endif
+
+#endif /* MAIN_H */
