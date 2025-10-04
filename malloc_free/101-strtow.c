156e4d83a2b488add81c236c23772fcac9d91622
ray201ss <ray201ss@example.com>
2025-10-04 19:21:40 +0000

    Fix 101-strtow.c: split into smaller functions for Betty compliance

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -1,35 +1,69 @@
 #include "main.h"
 #include <stdlib.h>
 
+int count_words(char *str);
+char *copy_word(char *str, int start, int len);
+void free_words(char **words, int count);
+
 /**
  * count_words - counts the number of words in a string
  * @str: string to count words in
- *
  * Return: number of words
  */
 int count_words(char *str)
 {
-	int i, count;
+	int i, count = 0;
 
-	count = 0;
 	for (i = 0; str[i] != '\0'; i++)
-	{
 		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 			count++;
-	}
 	return (count);
+}
+
+/**
+ * copy_word - copies a word from string
+ * @str: source string
+ * @start: start index
+ * @len: length of word
+ * Return: pointer to new word
+ */
+char *copy_word(char *str, int start, int len)
+{
+	char *word;
+	int j;
+
+	word = malloc((len + 1) * sizeof(char));
+	if (word == NULL)
+		return (NULL);
+	for (j = 0; j < len; j++)
+		word[j] = str[start + j];
+	word[j] = '\0';
+	return (word);
+}
+
+/**
+ * free_words - frees array of words
+ * @words: array to free
+ * @count: number of words to free
+ */
+void free_words(char **words, int count)
+{
+	int i;
+
+	for (i = 0; i < count; i++)
+		free(words[i]);
+	free(words);
 }
 
 /**
  * strtow - splits a string into words
  * @str: string to split
- *
  * Return: pointer to array of strings (words)
  */
 char **strtow(char *str)
 {
 	char **words;
-	int i, j, k, len, wc, word_len;
+	int i = 0, k = 0, wc, wlen;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
@@ -39,32 +73,21 @@ char **strtow(char *str)
 	words = malloc((wc + 1) * sizeof(char *));
 	if (words == NULL)
 		return (NULL);
-	i = 0;
-	k = 0;
 	while (str[i] != '\0')
 	{
 		if (str[i] != ' ')
 		{
-			word_len = 0;
-			j = i;
-			while (str[j] != ' ' && str[j] != '\0')
-			{
-				word_len++;
-				j++;
-			}
-			words[k] = malloc((word_len + 1) * sizeof(char));
+			wlen = 0;
+			while (str[i + wlen] != ' ' && str[i + wlen] != '\0')
+				wlen++;
+			words[k] = copy_word(str, i, wlen);
 			if (words[k] == NULL)
 			{
-				for (len = 0; len < k; len++)
-					free(words[len]);
-				free(words);
+				free_words(words, k);
 				return (NULL);
 			}
-			for (j = 0; j < word_len; j++)
-				words[k][j] = str[i + j];
-			words[k][j] = '\0';
 			k++;
-			i += word_len;
+			i += wlen;
 		}
 		else
 			i++;
