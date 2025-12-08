d0561b5a298247d6e1622f92fafdd9a4fa9d21fb
ray201ss <turki95r@gmail.com>
2025-12-08 11:54:32 +0300

    Fix broken files in variables_if_else_while

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints the alphabet in lowercase
+ * print_alphabet - prints the alphabet in lowercase
  *
- * Return: Always 0.
+ * Return: nothing
  */
-int main(void)
+void print_alphabet(void)
 {
-    char c = 'a';
+    char c;
 
-    while (c <= 'z')
-    {
-        putchar(c);
-        c++;
-    }
+    for (c = 'a'; c <= 'z'; c++)
+        _putchar(c);
 
-    putchar('\n');
-    return (0);
+    _putchar('\n');
 }
