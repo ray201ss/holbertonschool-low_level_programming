3ebbcb8dbda692748e4b9f45529a9d08b68cacab
ray201ss <turki95r@gmail.com>
2025-12-09 13:02:59 +0300

    Add task 6 print_line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -8,18 +8,16 @@
  */
 void print_line(int n)
 {
-	int i;
+    int i;
 
-	if (n <= 0)
-	{
-		_putchar('\n');
-		return;
-	}
+    if (n <= 0)
+    {
+        _putchar('\n');
+        return;
+    }
 
-	for (i = 0; i < n; i++)
-	{
-		_putchar('_');
-	}
+    for (i = 0; i < n; i++)
+        _putchar('_');
 
-	_putchar('\n');
+    _putchar('\n');
 }
